8271a44e446f4d9b096a0f1f3626a3c67f2ea482
Nguyễn Quang Duy <duynguyenquang412@gmail.com>
2023-07-25 09:47:31 +0700

    Initial Commit

---
+++ FoodieHub/FoodieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieHubApp.swift
+//  FoodieHub
+//
+//  Created by Duy Nguyen Quang on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
